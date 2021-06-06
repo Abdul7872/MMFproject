562705a4bc06156555e9a7972eae1ce439ead93c
Abdullah sk <aabdull787275@gmail.com>
2021-06-06 17:31:39 +0530

    MMF project commited

---
+++ script.js
@@ -0,0 +1,45 @@
+// pupil movements
+let pupils =document.getElementsByClassName("pupil");
+let pupilArr = Array.from(pupils)
+let pupilStartPoint = -85;
+let pupilRange = 160;
+
+
+// mouse X position
+let mouseXStratPoint = 0;
+let mouseXEndPoint = window.innerWidth;
+let currentXPosition = 0;
+let fracXValue = 0;
+
+//mouse Y position
+let mouseYStratPoint = 0;
+let mouseYEndPoint = window.innerHeight;
+let currentYPosition = 0;
+let fracYValue = 0;
+
+//let mouseXRange = mouseXEndPoint - mouseXStratPoint;
+
+const mouseMove = (event) => {
+    currentXPosition = event.clientX;
+    fracXValue = currentXPosition / mouseXEndPoint ;
+
+    currentYPosition = event.clientY;
+    fracYValue = currentYPosition / mouseYEndPoint;
+
+    //console.log( `${fracXValue}  ${fracYValue}`)
+
+    let pupilXCurrentPosition =pupilStartPoint + (fracXValue * pupilRange) ;
+    let pupilYCurrentPosition =pupilStartPoint + (fracYValue * pupilRange) ;
+    pupilArr.forEach((curPupil) =>{
+        curPupil.style.transform = `translate(${pupilXCurrentPosition}px, ${pupilYCurrentPosition}px)`; 
+    });
+
+}
+
+const windowResize =(event) =>{
+    mouseXEndPoint = window.innerWidth;
+    mouseYEndPoint = window.innerHeight;
+}
+
+window.addEventListener('mousemove', mouseMove);
+window.addEventListener('resize', windowResize);
